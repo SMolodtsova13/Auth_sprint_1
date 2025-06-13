05446cbe78b643bc7e6f4f979e49f3d60fdb2858
Svetlana <molodtsova201@yandex.ru>
2025-06-13 22:42:41 +0300

    Добавлен апи для эдпоинта   '/register'

--- auth_service/src/api/v1/auth.py
+++ auth_service/src/api/v1/auth.py
@@ -17,3 +17,23 @@ async def register_user(
     db: AsyncSession = Depends(get_session)
 ):
     return await AuthService.register_user(user_create, db)
+ 
+from fastapi import APIRouter, Depends, status
+from sqlalchemy.ext.asyncio import AsyncSession
+from schemas.entity import UserCreate, UserInDB
+from services.auth import AuthService
+from db.postgres import get_session
+
+router = APIRouter(prefix='/auth', tags=['auth'])
+
+
+@router.post(
+        '/register',
+        response_model=UserInDB,
+        status_code=status.HTTP_201_CREATED
+)
+async def register_user(
+    user_create: UserCreate,
+    db: AsyncSession = Depends(get_session)
+):
+    return await AuthService.register_user(user_create, db)
