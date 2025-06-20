ecefc77b415fbc571f993baf05f7a13fd28297fd
Molodtsova Svetlana <167689948+SMolodtsova13@users.noreply.github.com>
2025-06-20 19:20:56 +0300

    Update config.py

--- auth_service/core/config.py
+++ auth_service/core/config.py
@@ -1,26 +1,21 @@
+from pydantic_settings import BaseSettings
+
+
 class Settings(BaseSettings):
 
     # PostgreSQL
-    postgres_user: str = 'postgres'
-    postgres_password: str = 'postgres'
-    postgres_host: str = 'localhost'
-    postgres_port: int = 5432
-    postgres_db: str = 'auth_db'
-
+	@@ -12,26 +9,23 @@
     # Redis
     redis_host: str
     redis_port: int
     redis_db: int = 0
-
     # JWT
     jwt_secret: str
     jwt_algorithm: str = 'HS256'
     access_token_expire_minutes: int = 15
     refresh_token_expire_days: int = 7
-
     class Config:
         env_file = '.env'
-
     @property
     def pg_url(self):
         return (
@@ -29,3 +24,5 @@ class Settings(BaseSettings):
             f'{settings.postgres_host}:{settings.postgres_port}/'
             f'{settings.postgres_db}'
         )
+
+settings = Settings()
